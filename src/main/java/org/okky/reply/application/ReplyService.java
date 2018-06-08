0e0fb2ba0f3f48d56ae73e61bb8aae7e0c094773
hyunsu <coding8282@gmail.com>
2018-06-08 13:43:02 +0900

    write(), remove() 이후 이벤트를 큐에 날림

--- src/main/java/org/okky/reply/application/ReplyService.java
+++ src/main/java/org/okky/reply/application/ReplyService.java
@@ -1,29 +1,39 @@
 package org.okky.reply.application;
 
 import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.okky.reply.application.command.ModifyReplyCommand;
 import org.okky.reply.application.command.WriteReplyCommand;
 import org.okky.reply.domain.model.Reply;
 import org.okky.reply.domain.repository.ReplyRepository;
 import org.okky.reply.domain.service.ReplyConstraint;
+import org.okky.reply.domain.service.ReplyProxy;
+import org.okky.share.event.ReplyRemoved;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static lombok.AccessLevel.PRIVATE;
+import static org.okky.reply.application.ModelMapper.toReply;
+import static org.okky.reply.application.ModelMapper.toReplyWrote;
+
 @Service
 @Transactional
 @AllArgsConstructor
+@FieldDefaults(level = PRIVATE)
 public class ReplyService {
-    private ReplyRepository repository;
-    private ReplyConstraint constraint;
+    ReplyRepository repository;
+    ReplyConstraint constraint;
+    ReplyProxy proxy;
 
     public void write(WriteReplyCommand cmd) {
         String articleId = cmd.getArticleId();
 
         constraint.checkArticleExists(articleId);
         constraint.rejectWriteIfArticleBlocked(articleId);
-        Reply reply = ModelMapper.toReply(cmd);
+        Reply reply = toReply(cmd);
         repository.save(reply);
+        proxy.sendMessage(toReplyWrote(reply));
     }
 
     @PreAuthorize("@replySecurityInspector.isThisWriter(#cmd.replyId)")
@@ -42,6 +52,7 @@ public class ReplyService {
     public void remove(String replyId) {
         Reply reply = constraint.checkExistsAndGet(replyId);
         repository.delete(reply);
+        proxy.sendMessage(new ReplyRemoved(replyId));
     }
 
     public void removeForce(String replyId) {
