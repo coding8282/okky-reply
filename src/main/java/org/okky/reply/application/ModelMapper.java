ab60415687bc0e4df141099197ae9891bf383d22
hyunsu <coding8282@gmail.com>
2018-06-08 13:39:44 +0900

    ModelMapper/ReplyRepo에 새 메소드 추가
    - articleId로 지우는 기능.
    - reply -> replyWroteEvent로 바꿔주는 기능.

--- src/main/java/org/okky/reply/application/ModelMapper.java
+++ src/main/java/org/okky/reply/application/ModelMapper.java
@@ -2,6 +2,7 @@ package org.okky.reply.application;
 
 import org.okky.reply.application.command.WriteReplyCommand;
 import org.okky.reply.domain.model.Reply;
+import org.okky.share.event.ReplyWrote;
 
 public interface ModelMapper {
     static Reply toReply(WriteReplyCommand cmd) {
@@ -10,5 +11,18 @@ public interface ModelMapper {
                 cmd.getBody(),
                 cmd.getReplierId(),
                 cmd.getReplierName());
+    }
+
+    static ReplyWrote toReplyWrote(Reply r) {
+        return new ReplyWrote(
+                r.getId(),
+                r.getArticleId(),
+                r.getBody(),
+                r.getReplierId(),
+                r.getReplierName(),
+                r.getRepliedOn(),
+                r.getUpdatedOn(),
+                r.getAcceptedOn()
+        );
     }
 }
--- src/main/java/org/okky/reply/domain/repository/ReplyRepository.java
+++ src/main/java/org/okky/reply/domain/repository/ReplyRepository.java
@@ -1,18 +1,24 @@
 package org.okky.reply.domain.repository;
 
 import org.okky.reply.domain.model.Reply;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.RepositoryDefinition;
 import org.springframework.data.repository.history.RevisionRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 @RepositoryDefinition(domainClass = Reply.class, idClass = String.class)
 public interface ReplyRepository extends RevisionRepository<Reply, String, Long> {
     void save(Reply reply);
     Optional<Reply> findById(String id);
+    @Query("select r.id from Reply r where r.articleId=:articleId")
+    List<String> findIdsByArticleId(@Param("articleId") String articleId);
     boolean existsById(String id);
     boolean existsByArticleId(String articleId);
     long countByReplierId(String replierId);
     long countByArticleId(String articleId);
     void delete(Reply reply);
+    void deleteByArticleId(String articleId);
 }
