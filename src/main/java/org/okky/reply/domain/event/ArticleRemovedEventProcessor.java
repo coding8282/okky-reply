665fd7a3ca757c011b314ba80aa33df89b301378
hyunsu <coding8282@gmail.com>
2018-06-08 13:43:48 +0900

    ArticleRemovedEventProcessor 추가
    - 게시글이 삭제되면 관련된 모든 답글을 삭제.
    - 삭제된 답글 아이디를 큐에 전송.

---
+++ src/main/java/org/okky/reply/domain/event/ArticleRemovedEventProcessor.java
@@ -0,0 +1,29 @@
+package org.okky.reply.domain.event;
+
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.okky.reply.domain.repository.ReplyRepository;
+import org.okky.reply.domain.service.ReplyProxy;
+import org.okky.share.event.ArticleRemoved;
+import org.okky.share.event.RepliesRemoved;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Component
+@AllArgsConstructor
+@FieldDefaults(level = PRIVATE)
+class ArticleRemovedEventProcessor {
+    ReplyRepository repository;
+    ReplyProxy proxy;
+
+    @EventListener
+    void when(ArticleRemoved event) {
+        List<String> replyIds = repository.findIdsByArticleId(event.getArticleId());
+        repository.deleteByArticleId(event.getArticleId());
+        proxy.sendMessage(new RepliesRemoved(replyIds));
+    }
+}
