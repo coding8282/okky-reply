8184c5ab4df0cc2eccff4755001af13d8fed3b41
hyunsu <coding8282@gmail.com>
2018-06-11 02:50:41 +0900

    SNS 불필요한 설정 제거

--- src/main/java/org/okky/reply/SNSConfig.java
+++ src/main/java/org/okky/reply/SNSConfig.java
@@ -1,20 +1,14 @@
 package org.okky.reply;
 
 import com.amazonaws.services.sns.AmazonSNS;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 class SNSConfig {
-    @Value("${app.reply-topic}")
-    String topic;
-
     @Bean
     NotificationMessagingTemplate notificationMessagingTemplate(AmazonSNS amazonSNS) {
-        NotificationMessagingTemplate template = new NotificationMessagingTemplate(amazonSNS);
-        template.setDefaultDestinationName(topic);
-        return template;
+        return new NotificationMessagingTemplate(amazonSNS);
     }
 }
