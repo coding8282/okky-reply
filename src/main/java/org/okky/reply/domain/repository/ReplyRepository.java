d710727b567e04e2528773619ba8fc416cd2ae2a
hyunsu <coding8282@gmail.com>
2018-06-11 02:05:47 +0900

    새로운 rest endpoint 노출
    - 타 마이크로서비스가 사용하는 내부용 API

--- src/main/java/org/okky/reply/domain/repository/ReplyRepository.java
+++ src/main/java/org/okky/reply/domain/repository/ReplyRepository.java
@@ -1,6 +1,8 @@
 package org.okky.reply.domain.repository;
 
 import org.okky.reply.domain.model.Reply;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.RepositoryDefinition;
 import org.springframework.data.repository.history.RevisionRepository;
@@ -15,6 +17,8 @@ public interface ReplyRepository extends RevisionRepository<Reply, String, Long>
     Optional<Reply> findById(String id);
     @Query("select r.id from Reply r where r.articleId=:articleId")
     List<String> findIdsByArticleId(@Param("articleId") String articleId);
+    @Query("select distinct r.replierId from Reply r where r.articleId=:articleId order by r.replierId desc")
+    Page<String> findRepliersByArticleId(@Param("articleId") String articleId, Pageable pageable);
     boolean existsById(String id);
     boolean existsByArticleId(String articleId);
     long countByReplierId(String replierId);
---
+++ src/main/java/org/okky/reply/resource/ReplyResourceInternal.java
@@ -0,0 +1,33 @@
+package org.okky.reply.resource;
+
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.okky.reply.domain.repository.ReplyRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static lombok.AccessLevel.PRIVATE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RequestMapping("/internal")
+@RestController
+@AllArgsConstructor
+@FieldDefaults(level = PRIVATE)
+class ReplyResourceInternal {
+    ReplyRepository repository;
+
+    @GetMapping(value = "/articles/{articleId}/repliers", produces = APPLICATION_JSON_VALUE)
+    List<String> findRepliersById(
+            @PathVariable String articleId,
+            @PageableDefault(size = 200) Pageable pageable) {
+        Page<String> page = repository.findRepliersByArticleId(articleId, pageable);
+        return page.getContent();
+    }
+}
