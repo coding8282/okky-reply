f01573d4ab2e297ff5a78822c12510223de5cb5a
hyunsu <coding8282@gmail.com>
2018-06-15 16:20:15 +0900

    stg 환경에서의 배포스크립트 수정
    - spring cloud config 서버의 develop 브랜치에서 설정을 읽어오도록 설정

--- integration/stg/scripts/start-app.sh
+++ integration/stg/scripts/start-app.sh
@@ -2,4 +2,5 @@
 
 nohup java -jar \
 /home/ec2-user/okky-reply-1.0.0.jar \
---spring.profiles.active=prod > /dev/null 2> /dev/null < /dev/null &
+--spring.cloud.config.label=develop \
+> /dev/null 2> /dev/null < /dev/null &
