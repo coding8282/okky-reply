b8b63de322c8ded2ffd0395ac331584232b4eacf
hyunsu <coding8282@gmail.com>
2018-06-17 17:49:50 +0900

    Repository 리팩토링
    - 유지보수를 위하여 하드코딩된 문자열인 jpql을 지양하고 query method로 대체. 다만 쿼리 메소드
    는 이름이 길어지는 경향이 있으므로 java8의 default 메소드로 'alias'를 줌. 다만 이렇게
    해서 코드가 다소 증가하기는 했지만 맞는 것인지 사실 의문이므로 추후 유지보수에 문제가 있으면
    언제든지 원복해도 무방하다는 것이 개인적인 의견임.

--- src/main/java/org/okky/reply/domain/repository/ReplyVoteRepository.java
+++ src/main/java/org/okky/reply/domain/repository/ReplyVoteRepository.java
@@ -2,18 +2,21 @@ package org.okky.reply.domain.repository;
 
 
 import org.okky.reply.domain.model.ReplyVote;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.RepositoryDefinition;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 @RepositoryDefinition(domainClass = ReplyVote.class, idClass = String.class)
 public interface ReplyVoteRepository {
     void save(ReplyVote vote);
-    @Query("from ReplyVote v " +
-            "where v.replyId=:replyId " +
-            "and v.voterId=:voterId ")
-    Optional<ReplyVote> find(@Param("replyId") String replyId, @Param("voterId") String voterId);
+    boolean existsByReplyIdAndVoterId(String replyId, String voterId);
+    Optional<ReplyVote> findByReplyIdAndVoterId(String replyId, String voterId);
     void delete(ReplyVote vote);
+
+    default Optional<ReplyVote> find(String replyId, String voterId) {
+        return findByReplyIdAndVoterId(replyId, voterId);
+    }
+    default boolean wasAlreadyVoted(String replyId, String voterId) {
+        return existsByReplyIdAndVoterId(replyId, voterId);
+    }
 }
