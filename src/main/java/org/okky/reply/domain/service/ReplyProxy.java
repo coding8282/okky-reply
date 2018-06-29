2c69324df98a093ecb74519592dfb12e98fc0e11
hyunsu <coding8282@gmail.com>
2018-06-29 18:32:13 +0900

    로직을 간결하게 리팩토링
    - 토픽은 okky-로 시작하므로 메시지를 보내는 프로세스 간소화

--- src/main/java/org/okky/reply/domain/service/ReplyProxy.java
+++ src/main/java/org/okky/reply/domain/service/ReplyProxy.java
@@ -1,9 +1,9 @@
 package org.okky.reply.domain.service;
 
+import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.okky.share.event.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.okky.share.TopicUtil;
+import org.okky.share.event.DomainEvent;
 import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
 import org.springframework.stereotype.Service;
 
@@ -11,38 +11,15 @@ import static lombok.AccessLevel.PRIVATE;
 import static org.okky.share.util.JsonUtil.toJson;
 
 @Service
+@AllArgsConstructor
 @FieldDefaults(level = PRIVATE)
 public class ReplyProxy {
-    @Autowired
     NotificationMessagingTemplate snsClient;
 
-    @Value("${app.topic.reply-wrote}")
-    String topicReplyWrote;
-    @Value("${app.topic.reply-removed}")
-    String topicReplyRemoved;
-    @Value("${app.topic.replies-removed}")
-    String topicRepliesRemoved;
-    @Value("${app.topic.reply-pinned}")
-    String topicReplyPinned;
-
-    public void sendEvent(ReplyWrote event) {
-        send(topicReplyWrote, event);
-    }
-
-    public void sendEvent(ReplyRemoved event) {
-        send(topicReplyRemoved, event);
-    }
-
-    public void sendEvent(RepliesRemoved event) {
-        send(topicRepliesRemoved, event);
-    }
-
-    public void sendEvent(ReplyPinned event) {
-        send(topicReplyPinned, event);
-    }
-
-    // ------------------------------------
-    private void send(String topic, DomainEvent event) {
-        snsClient.sendNotification(topic, toJson(event), null);
+    public void sendEvent(DomainEvent event) {
+        String topic = TopicUtil.toTopicName(event);
+        String message = toJson(event);
+        String subject = null;
+        snsClient.sendNotification(topic, message, subject);
     }
 }
