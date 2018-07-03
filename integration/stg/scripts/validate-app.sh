6f3b2d370f20a6444a8348b73984487eb1bc56c6
hyunsu <coding8282@gmail.com>
2018-07-03 18:43:18 +0900

    배포스크립트 수정

--- integration/stg/scripts/validate-app.sh
+++ integration/stg/scripts/validate-app.sh
@@ -12,16 +12,12 @@ do
 	    echo "Service is up and running successfully after $counter attempts!"
         exit 0
     else
-        checkRetryExceeded
+        if [ $counter -gt $MAX_RETRY ]
+        then
+            echo "It's considered fail to start service."
+            exit 1
+        fi
         echo "Check service is running? - $counter/$MAX_RETRY"
         sleep $CHECK_INTERVAL
     fi
 done
-
-function checkRetryExceeded(){
-    if [ $counter -gt $MAX_RETRY ]
-    then
-        echo "It's considered fail to start service."
-        exit 1
-    fi
-}
